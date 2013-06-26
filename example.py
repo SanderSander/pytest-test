31a898848751f6d84c79b4c3550148dcf07b7825
Jacco Flenter <github@putpixel.nl>
2013-06-26 18:01:09 +0200

    initial commit

---
+++ example.py
@@ -0,0 +1,4 @@
+APP_REDIS_HOST = os.environ.get('WERCKER_REDIS_HOST', '127.0.0.1')
+APP_REDIS_PORT = os.environ.get('WERCKER_REDIS_PORT', '6379')
+REDIS_HOST = os.environ.get('WERCKER_REDIS_HOST', '127.0.0.1')
+REDIS_PORT = os.environ.get('WERCKER_REDIS_PORT', '6379')
